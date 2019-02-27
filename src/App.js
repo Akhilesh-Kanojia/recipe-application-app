c78cacd965c751a46dacda829a8a11c6f7811e45
Akhilesh Kanojia <kanojiaakhilesh2010@gmail.com>
2019-02-28 00:25:25 +0530

    second commite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+
+const API_KEY = "87d177a70ad9063e57b16cb1cc6d8f97";
 
 class App extends Component {
+
+   state = {
+     recipes: []
+   }
+
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&page=2`);
+    
+    const data = await api_call.json();
+     this.setState({
+       recipes: data.recipes
+     });
+   // console.log(this.state.recipes);
+   // console.log(data.recipes[0].recipe_id);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe}/>
+        {this.state.recipes.map((recipe) => {
+          return (
+            <div>
+              <img src={recipe.image_url} alt={recipe.title} />
+              <p key={recipe.recipe_id}>{recipe.title}</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
