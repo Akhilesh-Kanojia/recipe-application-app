58ed210b8051893fac0c7b7913c76bd4cd507faf
Akhilesh Kanojia <kanojiaakhilesh2010@gmail.com>
2019-03-10 00:25:20 +0530

    first commite
    recipe app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './components/Form';
+import Recipes from './components/Recipes';
 
 const API_KEY = "87d177a70ad9063e57b16cb1cc6d8f97";
 
@@ -16,12 +17,25 @@ class App extends Component {
     const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&page=2`);
     
     const data = await api_call.json();
+   
      this.setState({
        recipes: data.recipes
      });
    // console.log(this.state.recipes);
    // console.log(data.recipes[0].recipe_id);
   }
+
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes }); //If key and value name are same then u can kept a single name instead of "recipes: recipes"
+  }
+
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+
   render() {
     return (
       <div className="App">
@@ -29,14 +43,7 @@ class App extends Component {
          <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe}/>
-        {this.state.recipes.map((recipe) => {
-          return (
-            <div>
-              <img src={recipe.image_url} alt={recipe.title} />
-              <p key={recipe.recipe_id}>{recipe.title}</p>
-            </div>
-          )
-        })}
+        <Recipes recipes={this.state.recipes}/>
       </div>
     );
   }
